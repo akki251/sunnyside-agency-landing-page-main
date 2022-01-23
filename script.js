c14ac0ef60c176b04b05f3fa02dbacfa67673958
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-01-23 13:45:37 +0530

    complete comit

---
+++ script.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector(".nav-container");
+
+const ham = document.querySelector(".ham");
+
+const list = document.querySelectorAll(".nav-list li a");
+
+ham.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+});
+
+for (var i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", () => {
+    nav.classList.remove("nav-open");
+  });
+}
